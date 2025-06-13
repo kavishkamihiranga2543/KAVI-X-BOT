22463a509018713a87dcf2ac75c7e4f886721b2e
kavishkamihiranga2543 <kavishkamihiranga94@gmail.com>
2025-06-13 19:25:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-global.sessionID = process.env.SESSIONID || "ᴘᴜᴛ ʏᴏᴜʀ sᴇssɪᴏɴ ɪᴅ ʜᴇʀᴇ."
+global.sessionID = process.env.SESSIONID || "KAVI-X-SESSION-ID~IZJCgaqK#dRQJcOlwj9mWzc6V2XsQ7XTIdKx0tts13E8KjY6ECaM"
 global.botname = process.env.BOTNAME || "KAVI-X MD"
 global.ownernumber = process.env.OWNERNUMBER || "94702128378"
 global.ownername = process.env.OWNERNAME || "Cyber Kavi"
@@ -13,7 +13,7 @@ global.author = process.env.AUTHOR || "Cyber Kavi"
 global.creator = process.env.CREATOR || "94702128378@s.whatsapp.net"
 global.botprefix = process.env.BOTPREFIX || "."
 global.restart = process.env.RESTART || true
-global.mongoDB = process.env.MONGODB_URI || "ᴘᴜᴛ ʏᴏᴜʀ ᴍᴏɴɢᴏᴅʙ ᴜʀɪ ʜᴇʀᴇ."
+global.mongoDB = process.env.MONGODB_URI || "mongodb+srv://kavishkamihiranga94:kavishkamihiranga94@cluster0.skcgrvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
